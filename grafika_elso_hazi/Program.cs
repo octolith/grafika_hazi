6fc0fcdc109d13f3c52c3f1c052ffb249e3cf31b
Marcell Lévai <Marcell Lvai>
2017-10-26 09:45:56 +0200

    Add project files.

---
+++ grafika_elso_hazi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafika_elso_hazi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
